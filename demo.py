53f9cd185921b819a357b775ecff5f1a190d1440
Ubuntu <ubuntu@ip-172-31-18-242.us-east-2.compute.internal>
2023-04-12 04:27:42 +0000

    modified demo to take commandline args

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
 from viewformer.data.loaders import DatasetLoader
 from viewformer.utils.tensorflow import load_model
@@ -8,29 +9,46 @@ from viewformer.evaluate.evaluate_transformer_multictx import generate_batch_pre
 import viewformer.models.utils
 import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-setattr(viewformer.models.utils, 'load_lpips_model', lambda *args, **kwargs: None)
+def create_parser():
+    """Creates a parser from command-line arguments.
+    """
+    parser = argparse.ArgumentParser()
 
-plt.rcParams['figure.figsize'] = [12, 8]
+    parser.add_argument('--dataset_path', type=str, default='/home/ubuntu/novel-pov/viewformer/dataset', help='Path for getting dataset')
 
-test_loader = DatasetLoader(path='/home/ubuntu/viewformer/dataset', split='test')
+    parser.add_argument('--codebook_path', type=str, default='interiornet-codebook-th', help='Path for getting checkbook weights')
+    parser.add_argument('--transformer_path', type=str, default='interiornet-transformer-tf', help='Path for getting transformer weights')
 
-codebook = load_model('interiornet-codebook-th')
-transformer = load_model('interiornet-transformer-tf')
+    parser.add_argument('--output_dir', type=str, default='./output')
+    parser.add_argument('--output_name', type=str, default="foo", help='The name of the output')
 
-input_batch = test_loader[0]['frames'].astype('float32') / 255.
-plt.imshow(np_imgrid(input_batch)[0])
-plt.savefig('foo.png')
+    parser.add_argument('--batch_size', type=int, default=1, help='The number of images in a batch.')
 
-output_batch = np.clip(codebook(input_batch * 2 - 1)[0] / 2 + 0.5, 0, 1)
+    return parser
 
-plt.imshow(np_imgrid(output_batch)[0])
-plt.savefig('foo2.png')
+if __name__ == '__main__':
+    parser = create_parser()
+    args = parser.parse_args()
+    
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    setattr(viewformer.models.utils, 'load_lpips_model', lambda *args, **kwargs: None)
 
-images, cameras = test_loader[0]['frames'][np.newaxis, ...], test_loader[0]['cameras'][np.newaxis, ...]
-print(cameras)
-output = generate_batch_predictions(transformer, codebook, images, cameras)
-plt.imshow(np_imgrid(output['generated_images'][0])[0])
-plt.savefig('output.png')
+    plt.rcParams['figure.figsize'] = [12, 8]
+    test_loader = DatasetLoader(path=args.dataset_path, split='test')
 
-print("FUCK ME")
+    codebook = load_model(args.codebook_path)
+    transformer = load_model(args.transformer_path)
+
+    input_batch = test_loader[0]['frames'].astype('float32') / 255.
+
+    plt.imshow(np_imgrid(input_batch)[0])
+    plt.savefig('{}/{}_context.png'.format(args.output_dir, args.output_name))
+
+    output_batch = np.clip(codebook(input_batch * 2 - 1)[0] / 2 + 0.5, 0, 1)
+    plt.imshow(np_imgrid(output_batch)[0])
+    plt.savefig('{}/{}_codebook.png'.format(args.output_dir, args.output_name))
+
+    images, cameras = test_loader[0]['frames'][np.newaxis, ...], test_loader[0]['cameras'][np.newaxis, ...]
+    output = generate_batch_predictions(transformer, codebook, images, cameras)
+    plt.imshow(np_imgrid(output['generated_images'][0])[0])
+    plt.savefig('{}/{}_generated.png'.format(args.output_dir, args.output_name))
